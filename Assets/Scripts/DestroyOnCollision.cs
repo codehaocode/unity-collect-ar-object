50877503b808c18096f13be33b31ab7c47744483
Yuhao's Macbook <yuhao.zhong@code.berlin>
2020-06-27 14:33:50 +0200

    Refactor

--- Assets/Scripts/DestroyOnCollision.cs
+++ Assets/Scripts/DestroyOnCollision.cs
@@ -1,11 +1,7 @@
 ﻿using UnityEngine;
-using System.Collections;
-
 
 public class DestroyOnCollision : MonoBehaviour
 {
-    public AudioClip collectSound;
-
     public ParticleSystem burstingParticle;
 
     private AudioSource source;
@@ -18,12 +14,8 @@ public class DestroyOnCollision : MonoBehaviour
 
     void OnCollisionEnter(Collision col)
     {
-
-        // source.PlayOneShot(collectSound, 1F);
-        
         source.Play();
         burstingParticle.Play();
         Destroy(gameObject, 0.5F);
-
     }
 }
