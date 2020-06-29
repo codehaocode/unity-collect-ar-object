c2b73b79f4c9b4d5eaea5bfca2187f41684ed915
Y <y@CODEs-iMac.local>
2020-06-29 16:01:42 +0200

    Create one object by another object

--- Assets/Scripts/ARViewController.cs
+++ Assets/Scripts/ARViewController.cs
@@ -1,37 +1,36 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.XR.ARFoundation;
+﻿// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.XR.ARFoundation;
 
-public class ARViewController : MonoBehaviour
-{
-    [SerializeField] private GameObject arCamObject;
-    private Camera arCam;
-    private Vector3 arCamPosition;
-    public Vector3 objectPosition;
-    // List<GameObject> randomObjects = new List<GameObject>();
-    public GameObject randomObject;
+// public class ARViewController : MonoBehaviour
+// {
+//     [SerializeField] private GameObject arCamObject;
+//     private Camera arCam;
+//     private Vector3 arCamPosition;
+//     public Vector3 objectPosition;
+//     public GameObject randomObject;
 
-    void Start()
-    {
-        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+//     void Start()
+//     {
+//         Screen.sleepTimeout = SleepTimeout.NeverSleep;
         
-        randomObject = GameManager.Instance.SpawnObject();
+//         randomObject = GameManager.Instance.SpawnObject();
 
-        arCam = GetComponentInChildren<Camera>();
-    }
+//         arCam = GetComponentInChildren<Camera>();
+//     }
 
-    void Update()
-    {
-        arCamPosition = arCam.transform.position;
-        arCamObject.transform.SetPositionAndRotation(arCamPosition, Quaternion.identity);
+//     void Update()
+//     {
+//         arCamPosition = arCam.transform.position;
+//         arCamObject.transform.SetPositionAndRotation(arCamPosition, Quaternion.identity);
 
-        objectPosition = randomObject.transform.position;
-        objectPosition.y = arCamPosition.y;
-        randomObject.transform.SetPositionAndRotation(objectPosition, Quaternion.identity);
-    }
+//         objectPosition = randomObject.transform.position;
+//         objectPosition.y = arCamPosition.y;
+//         randomObject.transform.SetPositionAndRotation(objectPosition, Quaternion.identity);
+//     }
 
 
 
-}
+// }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] public GameObject[] spawnObjects;
-
-    public GameObject SpawnObject()
-    {
-        var index = UnityEngine.Random.Range(0, spawnObjects.Length);
-        var objectToSpawn = spawnObjects[index];
-    
-        Vector3 position = new Vector3((UnityEngine.Random.Range(-2F, 2F)), 0.0F, UnityEngine.Random.Range(1.5F, 2.5F));
-        return Instantiate(objectToSpawn, position, Quaternion.identity);
-    }
-
+    [SerializeField] private GameObject arCamObject;
+    private Camera arCam;
+    private Vector3 arCamPosition;
+    private Vector3 objectPosition;
+    private GameObject randomObject;
     public static GameManager Instance {
         get {
             if (instance == null)
@@ -38,6 +35,35 @@ public class GameManager : MonoBehaviour
         {
             instance = this;
         }
+
+        SpawnObject();
+
+        arCam = GetComponentInChildren<Camera>();
+
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+    void Update()
+    {
+        arCamPosition = arCam.transform.position;
+
+        arCamObject.transform.SetPositionAndRotation(arCamPosition, Quaternion.identity);
+
+        objectPosition = randomObject.transform.position;
+        
+        objectPosition.y = arCamPosition.y;
+        
+        randomObject.transform.SetPositionAndRotation(objectPosition, Quaternion.identity);
+    }
+
+    public void SpawnObject()
+    {
+        var index = UnityEngine.Random.Range(0, spawnObjects.Length);
+
+        var objectToSpawn = spawnObjects[index];
+
+        Vector3 position = new Vector3((UnityEngine.Random.Range(-2F, 2F)), 0.0F, UnityEngine.Random.Range(1.5F, 2.5F));
+        
+        randomObject = Instantiate(objectToSpawn, position, Quaternion.identity);
     }
 
 }
