e9568674083d79bf0368a60899a887b20019edcf
Y <y@CODEs-iMac.local>
2020-06-29 16:35:37 +0200

    Rotate the object to face camera

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,8 +9,6 @@ public class GameManager : MonoBehaviour
     [SerializeField] public GameObject[] spawnObjects;
     [SerializeField] private GameObject arCamObject;
     private Camera arCam;
-    private Vector3 arCamPosition;
-    private Vector3 objectPosition;
     private GameObject randomObject;
     public static GameManager Instance {
         get {
@@ -36,34 +34,34 @@ public class GameManager : MonoBehaviour
             instance = this;
         }
 
-        SpawnObject();
-
         arCam = GetComponentInChildren<Camera>();
+
+        SpawnObject();
 
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
     }
-    void Update()
-    {
-        arCamPosition = arCam.transform.position;
-
-        arCamObject.transform.SetPositionAndRotation(arCamPosition, Quaternion.identity);
-
-        objectPosition = randomObject.transform.position;
-        
-        objectPosition.y = arCamPosition.y;
-        
-        randomObject.transform.SetPositionAndRotation(objectPosition, Quaternion.identity);
-    }
-
     public void SpawnObject()
     {
         var index = UnityEngine.Random.Range(0, spawnObjects.Length);
 
         var objectToSpawn = spawnObjects[index];
 
-        Vector3 position = new Vector3((UnityEngine.Random.Range(-2F, 2F)), 0.0F, UnityEngine.Random.Range(1.5F, 2.5F));
+        Vector3 position = new Vector3((arCam.transform.position.x + UnityEngine.Random.Range(-1.5F, 1.5F)), arCam.transform.position.y , arCam.transform.position.z + UnityEngine.Random.Range(1F, 2F));
         
-        randomObject = Instantiate(objectToSpawn, position, Quaternion.identity);
+        randomObject = Instantiate(objectToSpawn,position, Quaternion.identity);
+    }
+
+    void Update()
+    {
+        arCamObject.transform.SetPositionAndRotation(arCam.transform.position, Quaternion.identity);
+
+        var objectPosition = randomObject.transform.position;
+        
+        objectPosition.y = arCam.transform.position.y;
+        
+        randomObject.transform.SetPositionAndRotation(objectPosition, Quaternion.identity);
+
+        randomObject.transform.LookAt(arCam.transform.position);
     }
 
 }
